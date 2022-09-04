bfd469dca1c5903e74ce15d2941e90147531bf76
DESKTOP-S1CKID2\Usuario <mmaio@proveedor.teco.com.ar>
2022-09-04 12:47:33 -0300

    Se suben los cambios

---
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -0,0 +1,24 @@
+package ar.edu.unq.po2.tp3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Counter {
+	
+	private List<Integer> numeros = new ArrayList<Integer>();
+	
+	public int contarNumerosPares() {
+		var contador = 0;
+		for (int i=0; i <= numeros.size(); i++ ) {
+			if (i % 2 == 0) {
+				contador ++;
+			}
+		}
+		return contador;
+	}
+
+	public void addNumber(int i) {
+		numeros.add(i);	
+	}
+
+}
