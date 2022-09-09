863d08de36095f368cf1bb5a8023386a3cd6bb72
DESKTOP-S1CKID2\Usuario <mmaio@proveedor.teco.com.ar>
2022-09-08 21:50:36 -0300

    Se sube correcciones de la clase Rectangulo.

--- src/ar/edu/unq/po2/tp3/Rectangulo.java
+++ src/ar/edu/unq/po2/tp3/Rectangulo.java
@@ -1,35 +1,53 @@
 package ar.edu.unq.po2.tp3;
 
 public class Rectangulo {
-	private Point x;
-	private Point y;
+	private Point esquinaInferiorDerecha;
+	private Point esquinaSuperiorIzquierda;
 	
-
-	public int calcularAreaDelRectangulo() {
-		return calcularAncho() * calcularAlto();
+	public Rectangulo(Point esquinaInfDer, Point esquinaSupIzq) {
+		this.setEsquinaInferiorDerecha(esquinaInfDer);
+		this.setEsquinaSuperiorIzquierda(esquinaSupIzq);
+	}
+	
+	public int perímetroDelCuadrado() {
+		return this.sumaDelLadoSuperior() + this.sumaDelLadoSuperior() + this.sumaDelLadoDerecho() + this.sumaDelLadoDerecho();
+	}
+	
+	public int sumaDelLadoSuperior() {
+		return this.getEsquinaSuperiorIzquierda().getX() + this.getEsquinaInferiorDerecha().getX();
+	}
+	
+	public int sumaDelLadoDerecho() {
+		return this.getEsquinaInferiorDerecha().getY() + this.getEsquinaSuperiorIzquierda().getY();
 	}
 
-	private int calcularAncho() {
-		return 0;
+	public int areaDelRectangulo() {
+		return this.alto() - this.ancho();
 	}
 
-	public Point getX() {
-		return x;
+	public int alto() {
+		return this.getEsquinaInferiorDerecha().getY() - this.getEsquinaSuperiorIzquierda().getY();
+	} 
+	
+	public int ancho() {
+		return this.getEsquinaInferiorDerecha().getX() - this.getEsquinaSuperiorIzquierda().getX();
 	}
 
-
-	public void setX(Point x) {
-		this.x = x;
+	public Point getEsquinaInferiorDerecha() {
+		return esquinaInferiorDerecha;
 	}
 
-
-	public Point getY() {
-		return y;
+	public void setEsquinaInferiorDerecha(Point esquinaInferiorDerecha) {
+		this.esquinaInferiorDerecha = esquinaInferiorDerecha;
 	}
 
-
-	public void setY(Point y) {
-		this.y = y;
+	public Point getEsquinaSuperiorIzquierda() {
+		return esquinaSuperiorIzquierda;
 	}
+
+	public void setEsquinaSuperiorIzquierda(Point esquinaSuperiorIzquierda) {
+		this.esquinaSuperiorIzquierda = esquinaSuperiorIzquierda;
+	}
+
 	
 }
