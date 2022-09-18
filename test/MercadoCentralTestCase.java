9f1b53259b816757999b357a0c0b68dddb9581aa
DESKTOP-S1CKID2\Usuario <mmaio@proveedor.teco.com.ar>
2022-09-18 16:12:41 -0300

    Se sube test del Mercado central.

---
+++ test/MercadoCentralTestCase.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tp5.Producto;
+import ar.edu.unq.po2.tp5.ProductoCooperativa;
+
+class MercadoCentralTestCase {
+	
+	Producto fideos;
+	Producto arroz;
+	ProductoCooperativa leche;
+	
+	@BeforeEach
+	public void setUp() {
+		fideos = new Producto(5.40f, 10);
+		arroz = new Producto(2.30f, 2);
+		leche = new ProductoCooperativa(3.20f, 5);
+	}
+	
+	@Test
+	void testGetPrecio() {
+		assertEquals(5.40f,fideos.getPrecio());
+		assertEquals(3.20f,leche.getPrecio());
+	}
+	
+	@Test
+	void testMontoAPagar() {
+		assertEquals(10.80f, fideos.montoAPagar(fideos));
+		assertEquals(7.7f, fideos.montoAPagar(arroz));
+		assertEquals(4.6f, arroz.montoAPagar(arroz));
+	}
+
+	@Test
+	void testDescuento() {
+		assertEquals(0.64000005f, leche.montoAPagar(leche));
+	}
+
+}
