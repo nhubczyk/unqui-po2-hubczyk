a1b74899f97dc908b7a4e371e0b36d82922f0448
DESKTOP-S1CKID2\Usuario <mmaio@proveedor.teco.com.ar>
2022-09-21 11:55:21 -0300

    Se suben cambios en la clase ActividadSemanal.

--- src/ar/edu/unq/po2/tp5/ActividadSemanal.java
+++ src/ar/edu/unq/po2/tp5/ActividadSemanal.java
@@ -1,18 +1,21 @@
 package ar.edu.unq.po2.tp5;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class ActividadSemanal {
 	private DíaDeLaSemana día;
-	private Date horaDeInicio;
+	private LocalTime horaDeInicio;
 	private Double duración;
 	private Deporte deporte;
 	
-	public ActividadSemanal(DíaDeLaSemana día, Date hora, Double duración, Deporte dep) {
+	public ActividadSemanal(DíaDeLaSemana día, LocalTime hora, Double duración, Deporte dep) {
 		this.setDía(día);
 		this.setHoraDeInicio(hora);
 		this.setDuración(duración);
@@ -29,7 +32,23 @@ public class ActividadSemanal {
 		
 	}
 	
+	public int horasTotalesDelMunicipio() {
+		List<ActividadSemanal> lista = new ArrayList<ActividadSemanal>();
+		return int resultado = lista.stream().reduce(0,(total, nuevo) -> acumulado + nuevo);
+	}
 	
+	public ActividadSemanal menorCostoDeUnaActividad(Deporte deporte) {
+		List<ActividadSemanal> lista = new ArrayList<ActividadSemanal>();
+	    return lista.stream().min(Comparator.comparing(Deporte.valueOf(deporte)::deporte));
+	}
+	
+	public Map<Actividad, ActividadSemanal> actividadMásEconómica() {
+		
+	}
+	
+	public void allActivity() {
+		
+	}
 	
 	public int costo() {
 		if((día == DíaDeLaSemana.LUNES) || día == DíaDeLaSemana.MARTES || día == DíaDeLaSemana.MIERCOLES) {
@@ -50,12 +69,12 @@ public class ActividadSemanal {
 		this.día = día;
 	}
 	
-	public Date getHoraDeInicio() {
+	public LocalTime getHoraDeInicio() {
 		return horaDeInicio;
 	}
 	
-	public void setHoraDeInicio(Date horaDeInicio) {
-		this.horaDeInicio = horaDeInicio;
+	public void setHoraDeInicio(LocalTime hora) {
+		this.horaDeInicio = hora;
 	}
 
 	public Double getDuración() {
