6fe32e9179cc3d9dd716cbd5c7d88194f422ee7b
DESKTOP-S1CKID2\Usuario <mmaio@proveedor.teco.com.ar>
2022-09-29 22:24:33 -0300

    Se eliminan clases para empezar devuelta

--- src/ar/edu/unq/po2/tp5/Agencia.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.po2.tp5;
-
-public interface Agencia {
-	
-	public int montoAPagar();
-	
-	public void registrarPago(Factura factura);
-	
-}
--- src/ar/edu/unq/po2/tp5/Costeable.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unq.po2.tp5;
-
-public interface Costeable {
-	
-	public Float getPrecio();
-	
-	public Float montoAPagar(Producto prod);
-}
